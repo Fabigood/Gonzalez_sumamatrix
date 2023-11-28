c27ce84cc6afe4811a30f61bb32cdfbdcff98592
Fabigood <fabio.gonzalez.fiallos@udla.edu.ec>
2023-11-28 15:06:26 -0500

    paso2

--- sumamatrix.c
+++ sumamatrix.c
@@ -11,7 +11,42 @@ int main()
     printf("Ingrese el numero de filas  \n");
     scanf("%d", &m);
 
-   
+    int Matrix[n][m];
+    
+
+
+    for (int i = 0; i < n; i++)
+    {           //for para imprimir la matrix  
+        printf("\n");
+
+        for (int j = 0; j < m; j++)
+        {
+            Matrix[i][j] = rand() % 100 + 1;        //agregamos para tener numeros randoms
+
+            printf("%d\t", Matrix[i][j]);
+        }
+    }
+
+         srand(time(NULL));
+    int p, q;
+        printf("\n");
+    printf("Ingrese el numero para filas segunda matrix  \n");
+    scanf("%d", &p); // se pide al usuario el tamana de la matrix
+    printf("Ingrese el numero de columnas  \n");
+    scanf("%d", &q);
+
+    int Matrix2[p][q];
+
+    for (int i = 0; i < p; i++)
+    {           //for para imprimir la matrix  
+        printf("\n");
+
+        for (int j = 0; j < q; j++)
+        {
+            Matrix2[i][j] = rand() % 100 + 1;        //agregamos para tener numeros randoms
+
+            printf("%d\t", Matrix2[i][j]);
+        }
     }
     
 
