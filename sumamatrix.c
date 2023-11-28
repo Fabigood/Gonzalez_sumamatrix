9917bb1b4823f4ba6334d52fe9c065001695d939
Fabigood <fabio.gonzalez.fiallos@udla.edu.ec>
2023-11-28 14:57:42 -0500

    1 paso

---
+++ sumamatrix.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    srand(time(NULL));
+    int n, m;
+    printf("Ingrese el numero de filas  \n");
+    scanf("%d", &n); // se pide al usuario el tamana de la matrix
+    printf("Ingrese el numero de filas  \n");
+    scanf("%d", &m);
+
+   
+    }
+    
+
+    return 0;
+}
